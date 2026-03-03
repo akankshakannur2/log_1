d9b0a6ef245fe0abc75187f85d12a69a730d5a3a
Akanksha Kannur <70768450+akankshakannur2@users.noreply.github.com>
2026-03-03 22:13:25 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,14 @@
+# write a program to find out the average of two numbers
+
+#take two numbers input from user
+a=input("enter first number:")
+b=input("enter second number:")
+
+#write the formula to find the average of two numbers
+sum = int(a)+int(b)
+avg = sum/ 2
+
+print("Average of two numbers will be",avg)
+
+
+
